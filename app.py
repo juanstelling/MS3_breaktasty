bb7d27984010b0241f7c26fde52c4945c9068686
juanstelling <juan.stelling@gmail.com>
2021-01-04 15:36:25 +0000

    Add signup template

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -18,10 +19,16 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
-# --------- HOME PAGE  ---------#
+# --------- HOME PAGE  --------- #
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+# --------- REGISTRATION  --------- #
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    return render_template("signup.html")
 
 
 @app.route("/get_recipes")
